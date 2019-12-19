9b8945a77f5621960984895cb6b4865b08515f0e
joker <948790131@qq.com>
2019-12-20 00:22:26 +0800

    111

--- src/cli.cpp
+++ src/cli.cpp
@@ -4,13 +4,16 @@
 #include<sys/socket.h>
 #include<stdio.h>
 #include<errno.h>
+#include<sys/types.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
 
 using namespace std;
 /*
 #PRINT(arg,...) printf("[%s:%d]:[%s] %s\n", __FILE__, __LINE__, __func___)
 */
 
-#define PRINT(fmt,...) printf(fmt, ##__VA_ARGS__)
+#define PRINT(fmt,...) printf(fmt,__VA_ARGS__)
 
 int main()
 {
@@ -36,25 +39,25 @@ int main()
 	sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if(sockfd<0)
 	{
-		PRINT("socket err[%d][%s]\n",errno, stderror(errno));
+		PRINT("socket err[%d][%s]\n",errno, strerror(errno));
 		return 0;
 	}
 
 
-	socketaddr_in addr;
+	sockaddr_in addr;
 	memset(&addr, 0, sizeof(addr));
 	addr.sin_family = AF_INET;
 	addr.sin_port  = htons(6666);
 	//addr.sin_addr = 
 	if(inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr)<=0)
 	{
-		PRINT("inet_pton err[%d][%s]\n",errno, stderror(errno));
+		PRINT("inet_pton err[%d][%s]\n",errno, strerror(errno));
 		return 0;
 	}
 
 	if(connect(sockfd, (sockaddr *)&addr, sizeof(addr)) < 0 )
 	{
-		PRINT("connect err[%d][%s]\n",errno, stderror(errno));
+		PRINT("connect err[%d][%s]\n",errno, strerror(errno));
 		close(sockfd);
 		return 0;	
 	}
@@ -64,7 +67,7 @@ int main()
 	
 	if(send(sockfd, msg, strlen(msg)) <0 )
 	{
-		PRINT("connect err[%d][%s]\n",errno, stderror(errno));
+		PRINT("connect err[%d][%s]\n",errno, strerror(errno));
 		close(sockfd);
 		return 0;			
 	}
@@ -74,5 +77,3 @@ int main()
 
 	return 0;
 }
-
-
